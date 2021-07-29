78ac224639ea5021cb96a7cae470bf06cc6b6c89
Erick Augusto <erick.dev.it@gmail.com>
2021-07-29 16:34:07 -0300

    Adicionando e configurando Fontes - Embedded Fonts

--- PokedexXF/PokedexXF/AssemblyInfo.cs
+++ PokedexXF/PokedexXF/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("sf-pro-display-regular.ttf", Alias = "FontRegular")]
+[assembly: ExportFont("sf-pro-display-medium.ttf", Alias = "FontMedium")]
+[assembly: ExportFont("sf-pro-display-bold.ttf", Alias = "FontBold")]
