2962d11d4b984c87bbe233a98199b885931dc2e3
Erick Augusto <erick.dev.it@gmail.com>
2021-08-12 20:26:45 -0300

    Definindo a orientação do android como Retrato

--- PokedexXF/PokedexXF.Android/MainActivity.cs
+++ PokedexXF/PokedexXF.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace PokedexXF.Droid
 {
-    [Activity(Label = "PokedexXF", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Pokedex", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
