072dd2b1803bd9fdb03ee88259d613a1ade9876f
ErickSilva2605 <erick.dev.it@gmail.com>
2022-04-03 16:28:39 -0300

    Ajustando Header da tela de detalhes

--- PokedexXF/PokedexXF/Views/DetailPage.xaml.cs
+++ PokedexXF/PokedexXF/Views/DetailPage.xaml.cs
@@ -10,7 +10,7 @@ namespace PokedexXF.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailPage : ContentPage
     {
-        const uint DURATION_ANIMATION_IMAGE = 500;
+        const uint DURATION_ANIMATION_IMAGE = 300;
         const uint DURATION_ANIMATION = 1000;
 
         public DetailPage(PokemonModel pokemon)
@@ -28,7 +28,9 @@ namespace PokedexXF.Views
 
         protected override async void OnAppearing()
         {
-            await gradientPokemonName.FadeTo(1, DURATION_ANIMATION, Easing.Linear);
+            await gradientPokemonName.FadeTo(0.3, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            await gradientPokemonName.FadeTo(0.7, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            await gradientPokemonName.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.TranslateTo(-300, 0, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.FadeTo(0.5, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.TranslateTo(-150, 0, DURATION_ANIMATION_IMAGE, Easing.Linear);
