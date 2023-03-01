b0aa5ed932b8460a8f216c0c5792864c87b98dbc
ErickSilva2605 <erick.dev.it@gmail.com>
2023-03-01 00:22:45 -0300

    Adicionando a classe MauiProgram e configurando fonts

---
+++ PokedexXF/PokedexXF/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace PokedexXF;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("sf-pro-display-regular.ttf", "FontRegular");
+                fonts.AddFont("sf-pro-display-medium.ttf", "FontMedium");
+                fonts.AddFont("sf-pro-display-bold.ttf", "FontBold");
+            });
+
+        return builder.Build();
+    }
+}
