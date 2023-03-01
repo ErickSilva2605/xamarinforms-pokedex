1c3be2691384767209081ed1baccf86625ea0acd
ErickSilva2605 <erick.dev.it@gmail.com>
2023-02-28 23:30:49 -0300

    Comentando todas as referencias do magic gradients

--- PokedexXF/PokedexXF/Views/DetailPage.xaml.cs
+++ PokedexXF/PokedexXF/Views/DetailPage.xaml.cs
@@ -22,24 +22,24 @@ namespace PokedexXF.Views
             imagePokemon.TranslationX = -300;
             contentBadgeType.TranslationY = -300;
             labelPokemonNumberName.TranslationY = -300;
-            gradientPatternCircle.Opacity = 0;
-            gradientPokemonName.Opacity = 0;
-            gradientPattern10x5.Opacity = 0;
+            //gradientPatternCircle.Opacity = 0;
+            //gradientPokemonName.Opacity = 0;
+            //gradientPattern10x5.Opacity = 0;
         }
 
         protected override async void OnAppearing()
         {
-            await gradientPokemonName.FadeTo(0.3, DURATION_ANIMATION_IMAGE, Easing.Linear);
-            await gradientPokemonName.FadeTo(0.7, DURATION_ANIMATION_IMAGE, Easing.Linear);
-            await gradientPokemonName.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            //await gradientPokemonName.FadeTo(0.3, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            //await gradientPokemonName.FadeTo(0.7, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            //await gradientPokemonName.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.TranslateTo(-300, 0, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.FadeTo(0.5, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.TranslateTo(-150, 0, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.TranslateTo(0, 0, DURATION_ANIMATION_IMAGE, Easing.Linear);
             await imagePokemon.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
-            await gradientPatternCircle.FadeTo(0.5, DURATION_ANIMATION_IMAGE, Easing.Linear);
-            await gradientPatternCircle.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
-            await gradientPattern10x5.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            //await gradientPatternCircle.FadeTo(0.5, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            //await gradientPatternCircle.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
+            //await gradientPattern10x5.FadeTo(1, DURATION_ANIMATION_IMAGE, Easing.Linear);
 
 
             await Task.WhenAll(
