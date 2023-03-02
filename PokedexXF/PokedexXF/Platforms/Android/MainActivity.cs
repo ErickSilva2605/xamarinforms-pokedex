686aca1645e8e09f80e87b053eaee44c6e961c59
ErickSilva2605 <erick.dev.it@gmail.com>
2023-03-02 13:33:55 -0300

    Ajuste na MainActivity

--- PokedexXF/PokedexXF/Platforms/Android/MainActivity.cs
+++ PokedexXF/PokedexXF/Platforms/Android/MainActivity.cs
@@ -1,10 +1,12 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace PokedexXF;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Label = "Pokedex",
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
